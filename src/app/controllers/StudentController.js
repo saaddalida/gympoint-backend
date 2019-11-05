8dc6d83c7e46877fa3b06cc8a1169f4dc355670c
saaddalida <dalidassaad@gmail.com>
2019-11-05 13:44:23 -0300

    feat: Create and update students

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,42 @@
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists.' });
+    }
+
+    const { id, name, email, age, weight, height } = await Student.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+
+  async update(req, res) {
+    const student = await Student.findByPk(req.params.id);
+
+    const { name, email, age, weight, height } = await student.update(req.body);
+
+    return res.json({
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+}
+
+export default new StudentController();
